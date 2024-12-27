9d7d1e5adb97f8fa9459d0b46025aa02f7f6a269
Roman <romanbezh@gmail.com>
2024-12-27 22:20:49 +0200

    added few simple tests for main page and status page

---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+import requests
+
+from pytest import fixture
+
+
+@fixture
+def url():
+    return "http://127.0.0.1:8000/"
+
+
+def test_main(url):
+    response = requests.get(url)
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+
+def test_status(url, uri='/status'):
+    response = requests.get(url + uri)
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
