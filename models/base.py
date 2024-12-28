f21b44823147b068e380d79a07fb2dff8b6df01f
Roman <romanbezh@gmail.com>
2024-12-28 10:48:35 +0200

    create Stock models

---
+++ models/base.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlmodel import Field, SQLModel
+
+
+class BaseModel(SQLModel, table=True):
+    """
+    Represents a base model for all models.
+
+    Attributes:
+        id (int): The primary key identifier for the record.
+        created_at (datetime): The timestamp when the record was created.
+        updated_at (datetime): The timestamp when the record was last updated.
+    """
+
+    id: int = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
---
+++ models/stock_models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlmodel import Field
+
+from .base import BaseModel
+
+
+class StockDividend(BaseModel):
+    """
+    Represents a stock dividend record.
+
+    Attributes:
+        symbol (str): The stock ticker symbol.
+        dividend_date (datetime): The date when the dividend is paid.
+        adjusted_amount (float): The adjusted amount of the dividend.
+        amount (float): The amount of the dividend.
+        record_date (str): The date when the company records the list of shareholders eligible for the dividend.
+        pay_date (str): The date when the dividend is paid.
+        declaration_date (str): The date when the dividend is declared.
+    """
+
+    symbol: str = Field(max_length=100, index=True)
+    dividend_date: Optional[datetime]
+    adjusted_amount: float
+    amount: float
+    record_date: str
+    pay_date: str
+    declaration_date: str
