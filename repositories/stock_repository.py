25873c1c046739cfd3d8a18dfd6aff201e2aca48
ibrg <romanbezh@gmail.com>
2024-12-29 15:06:07 +0200

    make template stock repository

---
+++ repositories/stock_repository.py
@@ -0,0 +1,13 @@
+from models.stock_models import Stock
+
+
+class StockRepository:
+
+    @staticmethod
+    def create_stock(stock: Stock):
+        pass
+
+    @staticmethod
+    def update_stock(stock_id: int, data: Stock):
+        pass
+
