a1d9581b6830de65a0cac3cec6d8aa6fda198c7b
ibrg <romanbezh@gmail.com>
2025-01-14 12:21:06 +0200

    remove delete db after close server

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from app.routers.main_routers import main_app
-from db.connection import init_db, close_db
+from db.connection import init_db
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()
     yield
-    close_db()
-    print("Shutting down the app")
+
 
 app = FastAPI(title="Dividents Flow", lifespan=lifespan)
 
